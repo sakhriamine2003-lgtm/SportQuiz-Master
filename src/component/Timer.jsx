01f65617bb34a7c3c56a153b20ca49cff574975a
jihanejador <jihanejador@gmail.com>
2026-03-18 14:53:00 +0000

    utilise useState

--- src/component/Timer.jsx
+++ src/component/Timer.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from "react";
 
 export default function Timer({duration =30, onTimeUp, currentQuestionIndex}){
-    
+    const [timeLeft, setTimeLeft] = useState(duration);
 }

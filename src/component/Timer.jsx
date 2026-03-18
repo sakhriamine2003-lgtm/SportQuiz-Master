982b865817bee6b0ace0b49bf4296e2ff84915f2
jihanejador <jihanejador@gmail.com>
2026-03-18 14:58:05 +0000

    creation la logic avec useEffect pour le timer change avec chaque question

--- src/component/Timer.jsx
+++ src/component/Timer.jsx
@@ -2,4 +2,19 @@ import {useState, useEffect} from "react";
 
 export default function Timer({duration =30, onTimeUp, currentQuestionIndex}){
     const [timeLeft, setTimeLeft] = useState(duration);
+
+    useEffect(()=>{
+        setTimeLeft(duration);
+    },[currentQuestionIndex, duration]);
+    
+    useEffect(()=>{
+        if(timeLeft===0){
+            onTimeUp();
+            return;
+        }
+        const timer = setInterval(()=>{
+            setTimeLeft((prev)=>prev -1);
+        },1000);
+        return ()=> clearInterval(timer);
+    },[timeLeft, onTimeUp]);
 }

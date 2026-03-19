4f3cc36804b722cd925ffa9c57dd597047b8a296
amine sakhri <sakhriaminee2003@gmail.com>
2026-03-19 13:33:48 +0100

    creation de formulaire pour ajouter

--- src/component/QuestionCard.jsx
+++ src/component/QuestionCard.jsx
@@ -27,3 +27,40 @@ const shuffleQuiz = () => {
       setAnswers(shuffledAnswers);
     }
   };
+   return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-6 rounded-2xl shadow-lg w-96 text-center">
+        <h2 className="text-xl font-bold mb-4">
+          {questions[currentIndex].question}
+        </h2>
+
+        <div className="space-y-3">
+          {answers.map((ans, index) => (
+            <button
+              key={index}
+              className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
+            >
+              {ans}
+            </button>
+          ))}
+        </div>
+
+<div className="flex gap-12">  
+        <button
+          onClick={handleNext}
+          className="mt-4 w-full  bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition"
+        >
+          Question suivante
+        </button>
+
+        <button
+          onClick={shuffleQuiz}
+          className=" w-10  "
+        >
+          <img src="https://cdn-icons-png.flaticon.com/128/3917/3917293.png" alt="logo" />
+        </button>
+      </div>
+    </div>
+    </div>
+  );
+
